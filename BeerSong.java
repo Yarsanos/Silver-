6fae3fd6fc39f8b5109cdef6e127abb8f8b32578
Yarsanos <yarsanos2@gmail.com>
2017-01-30 18:54:41 -0500

    Added Modified Java

--- BeerSong.java
+++ BeerSong.java
@@ -11,13 +11,29 @@ package beersong;
  */
 public class BeerSong {
 
+
+        
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Ninety_Nine_Bottles_of_Beer();
     }
-private void Ninety_Nine_Bottles_of_Beer(){
-    
+private static void Ninety_Nine_Bottles_of_Beer(){
+    int Bottle=99; 
+    while (Bottle>2){
+        System.out.println(Bottle + " bottles of beer on the wall," + Bottle + " bottles of beer");
+        System.out.println("take one down, pass it around,"+ Bottle+" bottles of beer on the wall.");
+        Bottle--;
+    }
+    if (Bottle==2) {
+        System.out.println(Bottle + " bottles of beer on the wall," + Bottle + " bottles of beer");
+        System.out.println("take one down, pass it around,"+ Bottle+" bottle of beer on the wall.");
+        Bottle--;
+    }
+    if (Bottle==1){
+        System.out.println(Bottle + " bottle of beer on the wall," + Bottle + " bottle of beer");
+        System.out.println("take one down, pass it around, no more bottles of beer on the wall.");
+    }
 }
 }
